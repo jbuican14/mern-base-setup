1ff987e3094f84284e2e0ded5c0da29a20aab95e
Bew <Bew@sysops-MBP.broadband>
2021-01-27 22:05:04 +0000

    Render templates at the root

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 // Set up the server to build out the node server application
 import express from 'express';
-import path from 'path';
 import devBundle from './devBundle'; //ONLY for dev
+import path from 'path';
+
+import template from './../template';
 
 const app = express();
 const CURRENT_WORKING_DIR = process.cwd();
+let port = process.env.PORT || 3008;
+
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
 devBundle.compile(app); //ONLY for dev
+
+app.get('/', (req, res) => {
+  res.status(200).send(template());
+});
+
+app.listen(
+  port,
+  (onStart = (err) => {
+    err ? console.log(err) : console.info('Server started on port %s.', port);
+  })
+);
