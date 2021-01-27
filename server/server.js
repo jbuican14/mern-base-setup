ff4b08eb814c2cef39df4808af4677bf74b792d1
Bew <Bew@sysops-MBP.broadband>
2021-01-27 22:13:31 +0000

    Add MongoDB database in a server.js

--- server/server.js
+++ server/server.js
@@ -2,18 +2,27 @@
 import express from 'express';
 import devBundle from './devBundle'; //ONLY for dev
 import path from 'path';
+import { MongoClient } from 'mongodb';
 
 import template from './../template';
 
 const app = express();
 const CURRENT_WORKING_DIR = process.cwd();
 let port = process.env.PORT || 3008;
+const url =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup';
 
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
 devBundle.compile(app); //ONLY for dev
 
 app.get('/', (req, res) => {
   res.status(200).send(template());
+});
+
+// Mongodb driver connects to the running MongoDB instance
+MongoClient.connect(url, (err, db) => {
+  console.log('Connected successfully to mongodb server');
+  db.close();
 });
 
 app.listen(
